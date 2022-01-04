7b459d0e07258b76fba39717a2c187ea0f368e88
rootrsk <ravishankar7050@gmail.com>
2022-01-05 00:26:17 +0530

    fixing cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-// const cors = require('cors')
-const cors = require('./src/utils/cors')
+const cors = require('cors')
+// const cors = require('./src/utils/cors')
 const port = process.env.PORT || 3001
 const io = require('socket.io')(http, {
   cors: {
@@ -14,30 +14,9 @@ require('./src/db/mongoose')
 const userRouter = require('./src/routes/user')
 const teacherRouter = require('./src/routes/teacher')
 const arduinoRouter = require('./src/routes/arduino')
-app.use(
-  function cors(req, res, next) {
-
-    // Website you wish to allow to connect
-    const allowedOrigins = [
-      "http://localhost:3000",
-    ];
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-    }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Set-Cookie,Authorization,authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of 
-    next();
-  }
-)
+app.use(cors())
 app.use(express.json())
-
+app.use(cors())
 app.use(userRouter)
 app.use(teacherRouter)
 app.use(arduinoRouter)
